a0ad210ba019d0e26ef0a5f02d8e72a32c82a330
Pascal <pk1998@gmx.de>
2020-06-20 16:11:42 +0200

    richtiges bit gesetzt

--- src/Memory.java
+++ src/Memory.java
@@ -166,7 +166,7 @@ public class Memory extends Thread {
 		int tmr = this.ram[0x1];
 		if (tmr == 255) {
 			this.ram[0x1] = 0;
-			this.ram[0xB] = 1;
+			this.ram[0xB] =+ 4;
 		}
 		else {
 		this.ram[0x1]++;
